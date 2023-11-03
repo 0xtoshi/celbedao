98a72fef8ad52c0398d5bf65076186197d67f81a
0xToshi <jsonpayment@gmail.com>
2023-11-04 04:32:49 +0700

    Remove edge

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-export const runtime = "edge"; // 'nodejs' (default) | 'edge'
 export default function handler(req, res) {
   res.status(200).json({ name: "John Doe" });
 }
